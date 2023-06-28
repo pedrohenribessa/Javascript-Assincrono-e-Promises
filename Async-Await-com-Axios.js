2622846fdd2fe310cc219cce1e0c3f411e102e11
pedrohenribessa <113377751+pedrohenribessa@users.noreply.github.com>
2023-06-28 11:03:49 -0300

    projeto finalizadnjkho

--- Async-Await-com-Axios.js
+++ Async-Await-com-Axios.js
@@ -2,12 +2,12 @@ const axios =  require ('axios')
 
 async function fecthRepos() {
 try {
-    const user = await axios.get('https://api.github.com/users/pedrohenribessa')
-    const repos = await axios.get(user.data.repos_url)
-    console.log(repos)
-}catch(e) {
+        const user = await axios.get('https://api.github.com/users/pedrohenribessa')
+        const repos = await axios.get(user.data.repos_url)
+        console.log(repos)
+    }catch(e) {
     console.log(e)
-}
+    }
 }
 
 fecthRepos()
